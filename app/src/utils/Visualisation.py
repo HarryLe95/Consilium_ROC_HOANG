76ae86cfd4ae1aef9e02f0d5c3562b3ac1a984e6
hson <lehoangsonsg@gmail.com>
2022-09-18 21:10:09 +0930

    Add weather files

--- app/src/utils/Visualisation.py
+++ app/src/utils/Visualisation.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 from datetime import timedelta 
 
-colour_dict = {0:'gold',
+label_colour_dict = {0:'gold',
                1:'orchid',
                2:'navy',
                3:'salmon',
@@ -13,19 +13,21 @@ colour_dict = {0:'gold',
                8:'mediumvioletred',
                9:'aqua'}
 
-weather_color_dict ={
-    "cloudcover":'dodgerblue',
-    "cloudcover_low":'lightskyblue',
-    "cloudcover_mid":'deepskyblue',
-    "cloudcover_high":'steelblue',
-    "shortwave_radiation":'beige',
-    "direct_radiation":'lawngreen',
-    "diffuse_radiation":'hotpink',
-    "direct_normal_irradiance":'peru'
+weather_colour_dict ={
+    "cloudcover":'red',
+    "cloudcover_low":'blue',
+    "cloudcover_mid":'white',
+    "cloudcover_high":'brown',
+    "shortwave_radiation":'black',
+    "direct_radiation":'purple',
+    "diffuse_radiation":'pink',
+    "direct_normal_irradiance":'orange'
 }
 
-def plot_label(ax: plt.axes, ax_id:int, label:pd.Series, start:str=None, end:str=None):
+def plot_label(ax: plt.axes, label:pd.Series|pd.DataFrame, start:str=None, end:str=None):
     #Slice dataframe based on start and end 
+    if isinstance(label, pd.DataFrame):
+        label = label.labels
     if start is None and end is None:
         label_df = label
     elif start is None and end is not None:
@@ -39,10 +41,16 @@ def plot_label(ax: plt.axes, ax_id:int, label:pd.Series, start:str=None, end:str
     for i in range(len(label_df)):
         x_start = label_df.index[i]
         x_end = x_start + timedelta(days=1)
-        colour = colour_dict[label_df[i]]
-        ax[ax_id].axvspan(x_start, x_end, ymin=0, ymax=1, color=colour, alpha = 0.5)
+        colour = label_colour_dict[label_df[i]]
+        ax.axvspan(x_start, x_end, ymin=0, ymax=1, color=colour, alpha = 0.5)
         
-def plot_data(ax: plt.Axes, ax_id:int, df:pd.Series, start:str=None, end:str=None, yticks=None, color:str='g', set_ylabel:bool=False):
+def plot_data(ax: plt.Axes, 
+              df:pd.Series, 
+              start:str=None, 
+              end:str=None, 
+              color:str='g',
+              size:int=20,
+              marker:str='.'):
     #Slice dataframe based on start and end 
     if start is None and end is None:
         plot_df = df
@@ -54,32 +62,48 @@ def plot_data(ax: plt.Axes, ax_id:int, df:pd.Series, start:str=None, end:str=Non
         plot_df = df.loc[start:end]
         
     #Plot overlaying features 
-    ax[ax_id].scatter(plot_df.index, plot_df.values, c = color, s=5, marker='.', label=plot_df.name)
-    ax[ax_id].grid()
-    if yticks is not None:
-        ax[ax_id].set_yticks(yticks)
-    if set_ylabel:
-        ax[ax_id].set_ylabel(plot_df.name)
-    ax[ax_id].legend()
+    ax.scatter(plot_df.index, plot_df.values, c=color, s=size, marker=marker, label=plot_df.name)
+    ax.grid()
     
 def plot_ROC(raw_df, well_name, weather_df=None, label_df=None, start=None, end=None, 
                     raw_features:list=["ROC_VOLTAGE","FLOW","PRESSURE_TH"],
-                    weather_features:list = ["cloudcover", "cloudcover_low", "cloudcover_mid", "cloudcover_high"]):
+                    weather_features:list = ["cloudcover", "cloudcover_low", "cloudcover_mid", "cloudcover_high",
+                    "shortwave_radiation", "direct_radiation","diffuse_radiation","direct_normal_irradiance"],
+                    label_colour_dict:dict = label_colour_dict, weather_colour_dict:dict = weather_colour_dict):
 
     fig, ax = plt.subplots(len(raw_features), figsize=(50,35), sharex=True)
     for idx, feature in enumerate(raw_features):
-        plot_data(ax, idx, raw_df[feature], start, end)
+        cloud_axis = ax[idx].twinx()
+        radiation_axis = ax[idx].twinx()
+        radiation_axis.spines.right.set_position(("axes", 1.03))
+        cloud_axis.set_ylabel("Cloud Cover", size=20)
+        radiation_axis.set_ylabel("Radiation", size=20)
+        cloud_axis.set_ylim(0,100)
+        radiation_axis.set_ylim(0,1000)
+        ax[idx].yaxis.set_tick_params(labelsize=20)
+        cloud_axis.yaxis.set_tick_params(labelsize=20)
+        radiation_axis.yaxis.set_tick_params(labelsize=20)
+
+        plot_data(ax[idx], raw_df[feature], start, end)
         if label_df is not None:
-            plot_label(ax,idx,label_df, start, end)
+            plot_label(ax[idx],label_df, start, end)
         if weather_df is not None:
             for weather_feature in weather_features:
-                plot_data(ax, idx, weather_df[weather_feature], start, end, color=weather_color_dict[weather_feature])
-        
-    # handleList = [plt.plot([], marker="o", ls="", color=color)[0] for color in colour_dict.values()]
+                if "cloud" in weather_feature:
+                    plot_data(cloud_axis, weather_df[weather_feature], start, end, color=weather_colour_dict[weather_feature],size=30, marker="X")
+                if "rad" in weather_feature:
+                    plot_data(radiation_axis, weather_df[weather_feature], start, end, color=weather_colour_dict[weather_feature],size=30, marker="D")
+        ax[idx].set_ylabel(feature, size=20)
+        ax[idx].legend(loc='upper left', prop={'size': 15})
+        cloud_axis.legend(loc='upper right', prop={'size': 15})
+        radiation_axis.legend(loc='lower left', prop={'size': 15})
 
-    legend = plt.legend(loc='right')
+    handleList = [plt.plot([], marker="o", ls="", color=color)[0] for color in label_colour_dict.values()]
+    legend = plt.legend(handleList, label_colour_dict.keys(), loc='right', prop={'size': 25})
     legend.get_frame().set_alpha(None)
     legend.get_frame().set_facecolor((0, 0, 1, 0.1))
+        
     fig.suptitle(f'{well_name}',fontsize=96)
     plt.xlabel("TS")
+    return fig 
     
